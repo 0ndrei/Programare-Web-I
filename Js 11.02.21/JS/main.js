e369693d6f1379dba1c6c28cdb3004772e06d9a2
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-02-17 19:26:38 +0200

    11.02

---
+++ Js 11.02.21/JS/main.js
@@ -0,0 +1,85 @@
+//Ex 1
+const a = prompt('Enter a number: ');
+if (a < 0) {
+    alert(-1);
+} else if (a == 0) {
+    alert(0);
+} else if (a > 0) {
+    alert(1);
+} else {
+    alert('Error');
+}
+
+//Ex 2
+/* var userAge = prompt('Crare este virtsa ta? ');
+if (userAge >= 14 && userAge <= 90) 
+    alert('True');
+    else alert('False'); */
+
+var userAge = prompt('Crare este virtsa ta? ');
+if (!(userAge >= 14 && userAge <= 90)) 
+    alert('False');
+    else alert('True');
+
+
+//Ex 3
+const validLoghin = '2020';
+const validPassword = '20';
+
+const inputLoghin = document.getElementById('form_login');
+const inputPassword = document.getElementById('form_password');
+const submit = document.getElementById('form_submit');
+
+const checkInput = () => {
+    if (inputLoghin.value !== validLoghin && inputPassword.value == validPassword) {
+        alert('Incorrect login');
+    }
+
+    if (inputLoghin.value == validLoghin && inputPassword.value !== validPassword) {
+        alert('Incorrect password');
+    }
+
+    if (inputLoghin.value !== validLoghin && inputPassword.value !== validPassword) {
+        alert('Validation error');
+    }
+}
+
+submit.addEventListener('click', () => {
+    checkInput();
+});
+
+//Ex 4
+const b = prompt("Enter minimum number: ");
+const c = prompt("Enter maximum number: ");
+
+let calc = () => {
+    let sum = 0;
+    for (let i = b; i <= c; i++) {
+        if (i % 2 == 0) {
+            sum = sum + i;
+        }
+    }
+    return sum;
+}
+
+console.log("Hello world!");
+
+document.body.style.backgroundColor = "cyan";
+
+setTimeout(function() {
+    document.body.style.backgroundColor = "yellow";
+}, 3000);
+
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+});
+
+document.getElementById("Bt1").addEventListener("click", function() {
+    document.body.style.backgroundColor = "red";
+});
+document.getElementById("Bt2").addEventListener("click", function() {
+    document.body.style.backgroundColor = "green";
+});
+document.getElementById("Bt3").addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue";
+});
