1407e9c9065edfe25dce87dbb50690921e38dc81
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-02-21 12:38:09 +0200

    Add files via upload

---
+++ Js Ex 3.3/JS/main.js
@@ -0,0 +1,35 @@
+console.log("Hello world!");
+
+document.body.style.backgroundColor = "blue";
+
+// EX.3.3
+
+//3.3(a)
+
+// const music = ['Djaz', 'Blues'];
+// console.log(music);
+
+//3.3(b)
+
+// const music = ['Djaz', 'Blues'];
+// music.push('Rock-n-Roll');
+// console.log(music);
+
+//3.3(c)
+
+// const music = ['Djaz', 'Blues', 'Rock-n-Roll'];
+// music[1] = 'Classic';
+// console.log(music);
+
+//3.3(d)
+
+// const music = ['Djaz', 'Classic', 'Rock-n-Roll'];
+// music.shift();
+// console.log(music);
+
+//3.3(e)
+
+const music = ['Djaz'];
+music.push('Classic', 'Rock-n-Roll');
+music.unshift('Rap', 'Raggy');
+console.log(music);
