90666d1bfe7e7644e9d9b2c1061bf8d994ecbc34
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-02-22 10:46:24 +0200

    Add files via upload

--- Js Ex 3.8/JS/main.js
+++ Js Ex 3.8/JS/main.js
@@ -2,17 +2,16 @@ document.body.style.backgroundColor = "blue";
 
 // EX.3.8
 
-
 const colors = ['red', 'green', 'blue'];
 
 function checkColor() {
     const a = prompt('Enter a color: ');
     if (a == 'red') {
-        alert(0);
+        alert(colors.indexOf("red"));
     } else if (a == 'green') {
-        alert(1);
+        alert(colors.indexOf("green"));
     } else if (a == 'blue') {
-        alert(2);
+        alert(colors.indexOf("blue"));
     } else {
         alert(-1);
     }
