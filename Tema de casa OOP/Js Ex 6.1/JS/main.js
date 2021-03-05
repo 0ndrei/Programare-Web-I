4a9587687e3eb3754046722ae6e77fddd75afe5e
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-03-05 13:14:23 +0200

    Tema de casa 04.03

--- Tema de casa OOP/Js Ex 6.1/JS/main.js
+++ Tema de casa OOP/Js Ex 6.1/JS/main.js
@@ -1,3 +1,14 @@
 document.body.style.backgroundColor = "red";
 
 // EX. 6.1
+
+class Person {
+    constructor() {
+        this.name = "Lungu";
+        this.surname = "Andrei";
+    }
+}
+
+let person = new Person();
+console.log(person.name);
+console.log(person.surname);
--- Tema de casa OOP/Js Ex 6.2/JS/main.js
+++ Tema de casa OOP/Js Ex 6.2/JS/main.js
@@ -1,3 +1,28 @@
 document.body.style.backgroundColor = "red";
 
 // EX. 6.2
+
+class Person {
+    constructor(name, surname) {
+        this._name = name;
+        this._surname = surname;
+    }
+
+    getName() {
+        return this._name;
+    }
+
+    getSurname() {
+        return this._surname;
+    }
+
+    setSurname(value) {
+        return this._surname = value;
+    }
+}
+
+let person = new Person('Lungu', 'Andrei');
+console.log(person.getName());
+console.log(person.getSurname());
+console.log(person.setSurname("Andreii"));
+console.log(person.getSurname());
