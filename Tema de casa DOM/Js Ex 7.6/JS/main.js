4730620dc216d48b4dcf38423b9420772478a7c5
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-03-11 14:30:23 +0200

    Delete Tema de casa DOM/Js Ex 7.6 directory

--- Tema de casa DOM/Js Ex 7.6/JS/main.js
+++
@@ -1,8 +0,0 @@
-// EX. 7.6
-
-let table = document.body.firstElementChild;
-
-for (let i = 0; i < table.rows.length; i++) {
-    let row = table.rows[i];
-    row.cells[i].style.backgroundColor = 'black';
-}
