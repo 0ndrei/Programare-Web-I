811ad1571b945c2cb6fc138384b381511f69f95c
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-03-11 14:29:57 +0200

    Tema de casa 11.03.2021

---
+++ Tema de casa DOM/Js Ex 7.15/JS/main.js
@@ -0,0 +1,7 @@
+
+// EX. 7.15
+
+
+for (var i=0; i< 64; i++){
+    document.getElementById("mainChessBoard").appendChild(document.createElement("div")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? '#ababab' : 'white';    
+}
---
+++ Tema de casa DOM/Js Ex 7.6/JS/main.js
@@ -0,0 +1,8 @@
+// EX. 7.6
+
+let table = document.body.firstElementChild;
+
+for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    row.cells[i].style.backgroundColor = 'black';
+}
