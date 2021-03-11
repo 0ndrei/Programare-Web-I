f138de73172c6b7b9bce25db63dcf82e48fa9c7b
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-03-11 14:30:14 +0200

    Delete Tema de casa DOM/Js Ex 7.15 directory

--- Tema de casa DOM/Js Ex 7.15/JS/main.js
+++
@@ -1,7 +0,0 @@
-
-// EX. 7.15
-
-
-for (var i=0; i< 64; i++){
-    document.getElementById("mainChessBoard").appendChild(document.createElement("div")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? '#ababab' : 'white';    
-}
