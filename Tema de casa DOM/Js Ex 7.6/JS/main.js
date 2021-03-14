b6857898a0c75442987e12258a50f368b18afb83
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-03-14 12:20:23 +0200

    Add files via upload

--- Tema de casa DOM/Js Ex 7.6/JS/main.js
+++ Tema de casa DOM/Js Ex 7.6/JS/main.js
@@ -1,5 +1,19 @@
 // EX. 7.6
+const tableRows = document.querySelectorAll('.Row');
 
-for (var i = 0; i < 64; i++) {
-  document.getElementById("sah").appendChild(document.createElement("div")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? 'black' : 'white';
+for (let row in tableRows) {
+    let tableCells = tableRows[row].cells;
+    if (row % 2 == 1) {
+        for (let cell in tableCells) {
+            if (cell % 2 == 0) {
+                tableCells[cell].classList.add('black');
+            }
+        }
+    } else {
+        for (let cell in tableCells) {
+            if (cell % 2 == 1) {
+                tableCells[cell].classList.add('black');
+            }
+        }
+    }
 }
