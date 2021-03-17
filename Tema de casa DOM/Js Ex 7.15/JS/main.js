85f2b2b94073b2458f782f57b45b72eb57436ef6
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-03-17 17:08:23 +0200

    Delete Tema de casa DOM directory

--- Tema de casa DOM/Js Ex 7.15/JS/main.js
+++
@@ -1,8 +0,0 @@
-// EX. 7.15
-
-let input = document.getElementById("input");
-input.addEventListener(`blur`, () => {
-  if (Number.isInteger(+input.value) == false) {
-    input.value = 0;
-  }
-});
